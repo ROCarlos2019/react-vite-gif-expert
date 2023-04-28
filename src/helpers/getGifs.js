b9a9f295c6675918e4503716e183ab90a7117073
ROCarlos2019 <roberts.pineda.rcp@gmail.com>
2023-04-28 11:40:01 -0600

    api key actualizada

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=Fb78aWqDvJoQ9gjfOLNHqOPQXCr5v2G0&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
